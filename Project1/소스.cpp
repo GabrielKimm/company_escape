46e31cf74ab958e69483193047d9ce9fbf1e588a
rlaalstn9601 <rlaalstn9601@github.com>
2019-04-07 04:06:55 +0900

    almost complete

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -6,12 +6,11 @@ struct Array {
 	int P;
 };
 Array* arr;
-//int score = 0;
 int max_score = 0;
-int tmp_score = 0;
 int n;
 
-
+void calc(int cur, int score);
+void recursion(int cur, int score);
 
 int main() {
 
@@ -20,16 +19,40 @@ int main() {
 		cout << "Exception Occurred" << endl;
 		return 0;
 	}
-
 	arr = (Array*)malloc(sizeof(Array) * n);
-
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i].T);
 		scanf("%d", &arr[i].P);
 	}
+	for (int i = 0; i < n; i++) {
+		calc(i + arr[i].T, arr[i].P);
+	}
+	cout << max_score;
+}
 
+void calc(int cur, int score) {
+	for (cur; cur < n; cur++) {
+		if (arr[cur].T == 1) {
+			score += arr[cur].P;
+		}
+		else {
+			recursion(cur, score);
+		}
+	}
+	recursion(cur, score);
+}
 
-
-	cout << endl << max_score << endl;
-
+void recursion(int cur, int score) {
+	if (cur + arr[cur].T > n || cur > n - 1) {
+		if (cur > n) {
+			return;
+		}
+		if (score > max_score) {
+			max_score = score;
+		}
+		return;
+	}
+	else {
+		recursion(cur + arr[cur].T, score + arr[cur].P);
+	}
 }
